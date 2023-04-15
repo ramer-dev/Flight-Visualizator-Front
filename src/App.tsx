2e15d60c828cf9983d99df74a56995e108f0130c
Ramon K. Shin <gml4009@gmail.com>
2023-04-16 01:01:01 +0900

    setting

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/navbar/NavBar';
+import Map from './components/map/map';
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
+      <Map/>
     </div>
   );
 }
