fbb6ff8cebe23b8f90dc45d9bad7e417819541f7
ramer-dev <gml4009@gmail.com>
2023-05-26 20:03:32 +0900

    set global styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import './App.css';
 import NavBar from 'components/navbar/NavBar';
 import Map from 'components/map/Map';
 import styled from '@emotion/styled';
+import { Global } from '@emotion/react';
+import {h1, h2, h3, h4} from 'style/global'
 
 function App() {
   return (
     <React.Fragment>
+      <Global styles={[h1, h2, h3, h4]}/>
       <NavBar></NavBar>
       <Map/>
     </React.Fragment>
---
+++ src/style/global.tsx
@@ -0,0 +1,34 @@
+import { css } from "@emotion/react";
+
+export const h1 = css`
+    h1{
+        font-size:24px;
+        line-height:24px;
+        margin:0;
+    }
+`
+
+export const h2 = css`
+    h2{
+        margin:0;
+        font-size:18px;
+        line-height:18px;
+
+    }
+`
+
+export const h3 = css`
+    h3{
+        margin:0;
+        font-size:14px;
+        line-height:14px;
+    }
+`
+
+export const h4 = css`
+    h4{
+        margin:0;
+        line-height:12px;
+        font-size:12px;
+    }
+`
