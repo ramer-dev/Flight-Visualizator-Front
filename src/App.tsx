7f4a98e392acfb3421ce7bc9e6a98c70b01bc6e3
ramer-dev <gml4009@gmail.com>
2023-05-26 19:15:14 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/navbar/NavBar';
+import NavBar from 'components/navbar/NavBar';
 import Map from 'components/map/Map';
 import styled from '@emotion/styled';
 
