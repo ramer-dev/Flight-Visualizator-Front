33cd97964ff290ef92585963412a29a8817f0ccd
RealKunse <51011193+RealKunse@users.noreply.github.com>
2023-06-15 22:42:45 +0900

    내일 다시하자

--- src/components/navbar/NavItem.tsx
+++ src/components/navbar/NavItem.tsx
@@ -1,9 +1,12 @@
 import styled from '@emotion/styled';
-import { useState } from 'react';
-import { motion } from 'framer-motion';
-import { page } from 'common/store/atom';
-import { useRecoilValue } from 'recoil';
-import { NavBarType } from 'common/type/NavBarType';
+import {useState} from 'react';
+import {motion} from 'framer-motion';
+import {page} from 'common/store/atom';
+import {useRecoilValue} from 'recoil';
+import {NavBarType} from 'common/type/NavBarType';
+import Box from '@mui/material/Box';
+import ButtonBase from '@mui/material/ButtonBase';
+
 type WrapperStyleType = {
     isChecked: boolean;
 }
@@ -17,22 +20,20 @@ const Wrapper = styled(motion.div)`
   width: 100%;
   gap: 5px;
   transition: 0.2s all ease;
-  /* padding: ${(props: WrapperStyleType) => (props.isChecked ? '10px 0' : '0')}; */
+    /* padding: ${(props: WrapperStyleType) => (props.isChecked ? '10px 0' : '0')}; */
   cursor: pointer;
   background: ${(props: WrapperStyleType) =>
-    (props.isChecked
-        ? 'linear-gradient(90deg, rgb(43, 111, 214) 0%, rgb(43, 111, 214) 4.9%,rgba(43, 111, 214, 0.28) 5%, rgba(255, 255, 255, 0) 50%)'
-        : null)};
+          (props.isChecked
+                  ? 'linear-gradient(90deg, rgb(43, 111, 214) 0%, rgb(43, 111, 214) 4.9%,rgba(43, 111, 214, 0.28) 5%, rgba(255, 255, 255, 0) 50%)'
+                  : null)};
+
+  &:hover {
+    background: linear-gradient(90deg, rgb(43, 111, 214) 0%, rgb(43, 111, 214) 4.9%, rgba(43, 111, 214, 0.28) 5%, rgba(255, 255, 255, 0) 50%)
+  }
 
   svg path {
-        fill : ${(props: WrapperStyleType) => (props.isChecked ? 'black' : '#9b9b9b')
-    };
-  &:hover{
-    background: 'linear-gradient(90deg, rgb(43, 111, 214) 0%, rgb(43, 111, 214) 4.9%,rgba(43, 111, 214, 0.28) 5%, rgba(255, 255, 255, 0) 50%)'
-  };
-
-
-    
+    fill: ${(props: WrapperStyleType) => (props.isChecked ? 'black' : '#9b9b9b')
+    }
 `
 
 const SubTitle = styled(motion.div)`
@@ -47,19 +48,26 @@ type NavItemType = {
 }
 // prop : 전달된 id, set ID함수
 const NavItem = (props: NavItemType) => {
+
     const Icon = props.icon;
-    
+
     const isClicked = props.title === useRecoilValue<NavBarType>(page)
     // const page = props.page;
     return (
         <Wrapper onClick={props.onclick} isChecked={isClicked}>
-            <Icon />
             {isClicked ?
-                <SubTitle
-                    initial={{ opacity: 0, y: -10 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 1, y: -10 }}>{props.title}</SubTitle>
-                : null}
+                <>
+                    <Icon/>
+                    <SubTitle
+                        initial={{opacity: 0, y: -10}}
+                        animate={{opacity: 1, y: 0}}
+                        exit={{opacity: 1, y: -10}}>{props.title}</SubTitle>
+                </>
+                : <>
+                    <Icon/>
+                    <SubTitle initial={{opacity: 0, y: -10}}>{props.title}</SubTitle>
+                </>
+            }
         </Wrapper>
     )
 }
