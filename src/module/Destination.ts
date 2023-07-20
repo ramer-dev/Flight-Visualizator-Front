de33ca92d9975e187594457b89d75e00a00c3142
ramer-dev <gml4009@gmail.com>
2023-07-20 23:10:39 +0900

    fix : 마킹 degree -> radian 변환

--- src/module/Destination.ts
+++ src/module/Destination.ts
@@ -4,15 +4,15 @@ import { convertToWGS } from 'module/DMS'
 
 export const Destination = (map: L.Map, origin: string | LatLngExpression | null, range: number | null, distance: number | null, line = true) => {
     if (!origin) return;
-    if (!range) return;
-    if (!distance) return;
+    if (range === null) return;
+    if (distance === null) return;
     if (!map) return;
     if (typeof origin === 'string') {
         if (origin?.includes('/')) {
             let origin_ = origin.split('/')
             const point: LatLngLiteral = { lat: +origin_[0], lng: +origin_[1] }
 
-            const target = L.GeometryUtil.destination(point, range, distance * 1852)
+            const target = L.GeometryUtil.destination(point, range * Math.PI / 180, distance * 1852)
 
             if (line) L.polyline([point, target]).addTo(map);
             L.marker(target).addTo(map);
@@ -22,7 +22,7 @@ export const Destination = (map: L.Map, origin: string | LatLngExpression | null
     } else if (typeof origin === 'object') {
         origin = origin as LatLngLiteral
         origin = { lat: convertToWGS(origin['lat']), lng: convertToWGS(origin['lng']) }
-        const target = L.GeometryUtil.destination(origin, range, distance * 1852)
+        const target = L.GeometryUtil.destination(origin, range * Math.PI / 180, distance * 1852)
         if (line) L.polyline([origin, target]).addTo(map);
         L.marker(target).addTo(map);
     }
