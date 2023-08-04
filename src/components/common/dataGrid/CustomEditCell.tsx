af1c4de68a2e3ed6c616b54dbcadbe0cbe756441
ramer-dev <gml4009@gmail.com>
2023-08-04 17:55:13 +0900

    fix : UI 행 추가삭제 버튼 삭제

--- src/components/common/dataGrid/CustomEditCell.tsx
+++ src/components/common/dataGrid/CustomEditCell.tsx
@@ -1,15 +1,21 @@
 import { GridRowId } from '@mui/x-data-grid';
-import React from 'react'
+import React, { useState } from 'react'
 interface Props {
     addFunction: (e: React.MouseEvent) => void;
-    delFunction: (i: GridRowId, e: React.MouseEvent) => void;
+    delFunction: (e: React.MouseEvent) => void;
     idx: GridRowId;
     isLast?: boolean;
 }
 function CustomEditCell({ addFunction, delFunction, idx, isLast}: Props) {
+    const [last, setLast] = useState(isLast);
+    const handlerFocus = (e: any) => {
+        addFunction(e)
+        setLast(false);
+    }
     return (
         <div>
-            {isLast ? <span onClick={addFunction}>+</span> : null}   <span onClick={(e: React.MouseEvent) => delFunction(idx, e)}>-</span>
+            {/* {last ? <span  onFocus={handlerFocus} tabIndex={0}>+</span> : null}   <span onClick={(e: React.MouseEvent) => delFunction(idx, e)}>-</span> */}
+            <span onClick={(e: React.MouseEvent) => delFunction( e)}>-</span>
         </div>
     )
 }
