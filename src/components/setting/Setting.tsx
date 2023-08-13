b9a56f571f1d40e572250c6392e91a4beb5edaf7
ramer-dev <gml4009@gmail.com>
2023-08-14 08:45:07 +0900

    feat: 설정 기능 기초공사

--- src/components/setting/Setting.tsx
+++ src/components/setting/Setting.tsx
@@ -1,17 +1,44 @@
+import styled from '@emotion/styled'
 import Title from 'components/common/Title'
 import React from 'react'
+import { SettingStateType } from './SettingStateType'
+
+const GridContainer = styled.div`
+  display:grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap:20px;
+`
+const Item = styled.div`
+  background-color:#ddd;
+  width:100px;
+  height:100px;
+  border-radius:50%;
+  line-height:100px;
+  text-align:center;
+  cursor:pointer;
+  &:hover {
+    background-color: #5096ff;
+    color:white;
+  }
+`
+
+
 
 function Setting() {
+  const SettingContext = React.createContext({})
+  const [settingState, setSettingState] = React.useState<SettingStateType>({})
   return (
-    <>
+    <SettingContext.Provider value={settingState}>
       <Title>환경설정</Title>
-      <div>표지소</div>
-      <div>항로</div>
-      <div>픽스점</div>
-      <div>주파수</div>
-      <div>섹터</div>
-      <div>구역</div>
-    </>
+      <GridContainer>
+        <Item>표지소</Item>
+        <Item>항로</Item>
+        <Item>픽스점</Item>
+        <Item>주파수</Item>
+        <Item>섹터</Item>
+        <Item>구역</Item>
+      </GridContainer>
+      </SettingContext.Provider>
   )
 }
 
---
+++ src/components/setting/SettingContext.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { SettingStateType } from "./SettingStateType";
+
+export const SettingContext = React.createContext<SettingStateType>({});
