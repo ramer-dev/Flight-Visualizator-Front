c0a3342b2780399fd960cf765c4b838385d07fd4
ramer-dev <gml4009@gmail.com>
2023-08-26 00:17:57 +0900

    fix : 아이템 변경 시 인풋 값 변경

--- src/components/setting/frequency/FrequencyEdit.tsx
+++ src/components/setting/frequency/FrequencyEdit.tsx
@@ -8,17 +8,27 @@ import { SettingStateType } from '../SettingStateType'
 
 function FrequencyEdit() {
     const settingState = useRecoilValue<SettingStateType>(setting)
-    const {data} = useGetSite();
+    const { data } = useGetSite();
 
     const siteData = data.map(t => t.siteName);
-    const freq = React.useRef<string>(settingState.data.label)
-    const site = React.useRef<string>(settingState.data.site)
+    const freq = React.useRef<HTMLInputElement>(null)
+    const site = React.useRef<HTMLInputElement>(null)
+
+    React.useEffect(() => {
+        console.log(settingState);
+
+        if (site?.current && freq?.current && settingState?.data) {
+            freq.current.value = settingState.data.label;
+            site.current.value = settingState.data.site;
+        }
+    }, [settingState])
+
 
     return (
         <>
             <ScreenTitle text={'주파수'} />
-            <TextField label="주파수" value={freq.current} type="number"/>
-            <TextField label="표지소" value={site.current} error={!siteData.includes(site.current)}/>
+            <TextField label="주파수" inputRef={freq} defaultValue={settingState.data.label} type="number" />
+            <TextField label="표지소" inputRef={site} defaultValue={settingState.data.site} error={site?.current ? !siteData.includes(site.current.value) : false} />
         </>
     )
 }
