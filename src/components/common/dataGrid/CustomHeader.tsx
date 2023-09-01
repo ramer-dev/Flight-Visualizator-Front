fd09cdecdf8570506994a9a7690193b0959fc7a1
ramer-dev <gml4009@gmail.com>
2023-09-01 16:34:04 +0900

    feat : ocr 버튼 생성 및 확장자 제한

--- src/components/common/dataGrid/CustomHeader.tsx
+++ src/components/common/dataGrid/CustomHeader.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled'
-import { Button, Divider, TextField } from '@mui/material'
+import { Button, Divider, TextField, Tooltip } from '@mui/material'
 import React, { useEffect } from 'react'
 import { MuiFileInput } from 'mui-file-input'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
@@ -10,7 +10,7 @@ import dayjs from 'dayjs';
 import { title } from 'process';
 import CustomFileInput from '../CustomFileInput';
 import { postRoute } from 'common/service/fileService';
-
+import PsychologyIcon from '@mui/icons-material/Psychology';
 const Container = styled.div`
     width:100%;
 `
@@ -26,7 +26,7 @@ interface Props {
     setTitleData: (item: FlightList) => void;
     edit?: boolean;
     submitted: boolean;
-    setSubmitted: (b : boolean) => void;
+    setSubmitted: (b: boolean) => void;
 }
 
 
@@ -34,33 +34,35 @@ interface Props {
 function CustomHeader({ titleData, setTitleData, edit, submitted, setSubmitted }: Props) {
 
     const [routeFile, setRouteFile] = React.useState<File>();
-    const [resultFile, setResultFile] = React.useState<File[]>([]);
+    const [ocrFile, setOCRFile] = React.useState<File[]>();
+    // const [resultFile, setResultFile] = React.useState<File[]>([]);
     const [flightData, setFlightData] = React.useState<FlightList>(titleData)
     const handleFileChange = (newFile: any) => {
         setRouteFile(newFile);
     }
 
-    const resultFileChange = (newFile: any) => {
-        setResultFile(newFile);
+    const HandleOCRFileChange = (newFile: any) => {
+        setOCRFile(newFile);
     }
     const titleRef = React.useRef<HTMLInputElement>();
     const typeRef = React.useRef<HTMLInputElement>();
     const routeRef = React.useRef<HTMLInputElement>();
     const dateRef = React.useRef<HTMLInputElement>(null);
+    const ocrRef = React.useRef<HTMLInputElement>(null);
 
     React.useEffect(() => {
 
         if (titleData) {
             if (titleRef.current) titleRef.current.value = titleData.testName;
             if (typeRef.current) typeRef.current.value = titleData.testType;
-            // if (routeRef.current) routeRef.current.value = 'wtf'
+            if (routeRef.current) routeRef.current.accept = 'application/vnd.ms-excel, .csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
         }
     }, [titleData])
 
     const handleSubmit = async () => {
         if (titleRef.current && typeRef.current && routeRef.current && dateRef.current) {
-            if(routeFile){
-                const {filePath} = await postRoute(routeFile);
+            if (routeFile) {
+                const { filePath } = await postRoute(routeFile);
                 setTitleData({ ...titleData, testName: titleRef.current.value, testType: typeRef.current.value, testDate: dateRef.current.value, testRoute: filePath })
             } else {
                 setTitleData({ ...titleData, testName: titleRef.current.value, testType: typeRef.current.value, testDate: dateRef.current.value })
@@ -71,21 +73,30 @@ function CustomHeader({ titleData, setTitleData, edit, submitted, setSubmitted }
     const handleCancel = () => {
         setSubmitted(false);
     }
+
+    const handleOCRClick = () => {
+        if (ocrRef.current) {
+            ocrRef.current.accept = "image/png, image/jpeg, application/pdf"
+            ocrRef.current.click();
+        }
+    }
     return (
         <Container>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <InputWrapper>
                     <TextField inputRef={titleRef} helperText="비행검사명" size='small' disabled={!edit || submitted}></TextField>
                     <TextField inputRef={typeRef} sx={{ width: 128 }} helperText="검사유형" size='small' disabled={!edit || submitted}></TextField>
-                    <MuiFileInput label="항적 파일" value={routeFile} onChange={handleFileChange} disabled={!edit || submitted} size='small' helperText='항적 파일' sx={{ width: 256 }} inputRef={routeRef} hideSizeText getInputText={(value:any) => { return value?.size && value.name ? value.name : titleData?.testRoute ? '경로 입력됨' : '' }} />
+                    <MuiFileInput label="항적 파일" value={routeFile} onChange={handleFileChange} disabled={!edit || submitted} size='small' helperText='항적 파일' sx={{ width: 256 }} inputRef={routeRef} hideSizeText getInputText={(value: any) => { return value?.size && value.name ? value.name : titleData?.testRoute ? '경로 입력됨' : '' }} />
                     {/* <MuiFileInput value={resultFile} onChange={resultFileChange} size='small' placeholder='결과지 파일' sx={{ width: 256 }} inputRef={resultRef} hideSizeText multiple  /> */}
                     {/* <CustomFileInput id={1}/> */}
                     <DatePicker format="YYYY-MM-DD" inputRef={dateRef} slotProps={{ textField: { size: 'small', helperText: '검사일자' } }} value={titleData?.testDate ? dayjs(titleData.testDate) : null} sx={{ width: 156 }} disabled={!edit || submitted} />
-                    {edit ?
+                    {edit && <Tooltip title={'개발중'}><Button sx={{ height: 40 }} onClick={handleOCRClick} startIcon={<PsychologyIcon fontSize={'large'} />}>OCR</Button></Tooltip>}
+                    <MuiFileInput value={ocrFile} multiple onChange={HandleOCRFileChange} sx={{ display: 'none' }} inputRef={ocrRef} />
+                    {edit &&
                         <>
-                            <Button onClick={handleSubmit} disabled={!edit || submitted} sx={{height:40}}>확인</Button>
-                            <Button onClick={handleCancel} color="error" disabled={!edit || !submitted} sx={{height:40}}>취소</Button>
-                        </> : null
+                            <Button onClick={handleSubmit} disabled={!edit || submitted} sx={{ height: 40 }}>확인</Button>
+                            <Button onClick={handleCancel} color="error" disabled={!edit || !submitted} sx={{ height: 40 }}>취소</Button>
+                        </>
                     }
                 </InputWrapper>
             </LocalizationProvider>
